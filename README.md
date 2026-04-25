ed583e50e63b45047587e176080040a599041f75
rsilva9 <ramasilva909@gmail.com>
2026-04-25 17:19:19 -0300

    init: initial setup (folder structure, libraries, config)

---
+++ README.md
@@ -0,0 +1,2 @@
+# MAP BADGER
+Homebrew app for linking maps between each other!
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "tailwindcss";
+
+function App(){
+
+  return (
+    <>
+      <h1 className="text-[100px]">HELLO!</h1>
+    </>
+  )
+}
+
+export default App;
